f66fd8546d0d5695097f526942330f5f6ca25645
Smerchizi <smerch1533@gmail.com>
2023-11-11 18:29:42 +0200

    Homework 11.11

--- src/telran/interview/InterviewTasks.java
+++ src/telran/interview/InterviewTasks.java
@@ -1,8 +1,12 @@
 package telran.interview;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class InterviewTasks {
+
 /**
  * 
  * @param ar array of integer numbers
@@ -10,17 +14,42 @@ public class InterviewTasks {
  * @return true if the given array contains two numbers, the sum of which equals the given sum value
  */
 	public static boolean isSum2(int [] ar, int sum) {
-	//TODO  should be done with O[N]
+		HashSet<Integer> seenNumbers = new HashSet<>();
+		for (int num : ar) {
+			int complement = sum - num;
+
+			if (seenNumbers.contains(complement)) {
+				return true;
+			}
+
+			seenNumbers.add(num);
+		}
+
 		return false;
-}
+	}
+
 	/**
 	 * 
 	 * @param ar array of integer numbers
 	 * @return maximal positive number having negative number with the same absolute value
 	 */
 	public static int getMaxPositiveWithNegativeValue (int ar[]) {
-		//TODO should be done with O[N]
-		return -1;
+		int pos = Integer.MIN_VALUE;
+		int testNum = 0;
+		HashSet<Integer> seenNumbers = new HashSet<>();
+		for (int num : ar) {
+			testNum = num * -1;
+			if (seenNumbers.contains(testNum) && testNum > pos ) {
+				pos = testNum;
+			}
+			if (num < 0 && num > pos){
+				pos = num;
+
+			}
+			seenNumbers.add(num);
+		}
+
+		return pos;
 	}
 	
 	/**
@@ -29,11 +58,21 @@ public class InterviewTasks {
 	 * @return Map where key - string, value - number of occurrences in the array
 	 */
 	public static HashMap<String, Long> getMapOccurrences(String[] strings) {
-		//TODO should be done with complexity O[N]
-		//Optionally, additional bonus if you apply the method "merge" of the interface Map
-		//try to understand this method https://docs.oracle.com/javase/8/docs/api/java/util/Map.html#merge-K-V-java.util.function.BiFunction- 
-		//it uses Functional interface BiFunction
-		return null;
+		HashMap<String, Long> map = new HashMap<>();
+		for (String s : strings) {
+			boolean res = map.containsKey(s);
+			if (res == false) {
+				map.put(s, 1L);
+			} else {
+				Long value = map.get(s);
+				value++;
+				map.replace(s, value);
+			}
+
+		}
+
+		return map;
 	}
-	
+
+
 }
