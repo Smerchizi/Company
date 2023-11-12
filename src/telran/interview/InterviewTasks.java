bbbf398f8204b082e5217c852db8a59f6e76bcc9
Smerchizi <smerch1533@gmail.com>
2023-11-12 16:05:36 +0200

    Homework 12.11

--- src/telran/interview/InterviewTasks.java
+++ src/telran/interview/InterviewTasks.java
@@ -1,78 +1,115 @@
 package telran.interview;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
-
 
 public class InterviewTasks {
-
-/**
- * 
- * @param ar array of integer numbers
- * @param sum integer number
- * @return true if the given array contains two numbers, the sum of which equals the given sum value
- */
-	public static boolean isSum2(int [] ar, int sum) {
-		HashSet<Integer> seenNumbers = new HashSet<>();
-		for (int num : ar) {
-			int complement = sum - num;
-
-			if (seenNumbers.contains(complement)) {
-				return true;
-			}
-
-			seenNumbers.add(num);
-		}
-
-		return false;
-	}
-
 	/**
-	 * 
+	 *
+	 * @param ar array of integer numbers
+	 * @param sum integer number
+	 * @return true if the given array contains two numbers, the sum of which equals the given sum value
+	 */
+	public static boolean isSum2(int [] ar, int sum) {
+
+		boolean running = true;
+		HashSet<Integer> setHelper = new HashSet<>();
+		int i = 0;
+		while(i < ar.length && running) {
+			if(setHelper.contains(sum - ar[i])) {
+				running = false;
+			} else {
+				setHelper.add(ar[i]);
+				i++;
+			}
+		}
+		return !running;
+	}
+	/**
+	 *
+	 * @param ar array of integer numbers
+	 * @param sum integer number
+	 * @return true if the given array contains two numbers, the sum of which equals the given sum value
+	 */
+	public static boolean isSum2N2(int[] ar, int sum) {
+		int i = 1;
+		int j = 0;
+		boolean running = true;
+		while(i < ar.length && running) {
+			j = i - 1;
+			while(j >= 0 ) {
+				if(ar[j] + ar[i] == sum) {
+					running = false;
+					break;
+				}
+				j--;
+			}
+			i++;
+		}
+		return !running;
+	}
+	/**
+	 *
 	 * @param ar array of integer numbers
 	 * @return maximal positive number having negative number with the same absolute value
 	 */
 	public static int getMaxPositiveWithNegativeValue (int ar[]) {
-		int pos = Integer.MIN_VALUE;
-		int testNum = 0;
-		HashSet<Integer> seenNumbers = new HashSet<>();
-		for (int num : ar) {
-			testNum = num * -1;
-			if (seenNumbers.contains(testNum) && testNum > pos ) {
-				pos = testNum;
-			}
-			if (num < 0 && num > pos){
-				pos = num;
+		int res = -1;
+		HashSet<Integer> setHelper = new HashSet<>();
 
+		for(int num: ar) {
+			if(setHelper.contains(-num)) {
+				res = Math.max(res, Math.abs(num));
+			} else {
+				setHelper.add(num);
 			}
-			seenNumbers.add(num);
 		}
-
-		return pos;
+		return res;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param strings - array of strings
 	 * @return Map where key - string, value - number of occurrences in the array
 	 */
 	public static HashMap<String, Long> getMapOccurrences(String[] strings) {
-		HashMap<String, Long> map = new HashMap<>();
-		for (String s : strings) {
-			boolean res = map.containsKey(s);
-			if (res == false) {
-				map.put(s, 1L);
-			} else {
-				Long value = map.get(s);
-				value++;
-				map.replace(s, value);
-			}
 
+		//Optionally, additional bonus if you apply the method "merge" of the interface Map
+		//try to understand this method https://docs.oracle.com/javase/8/docs/api/java/util/Map.html#merge-K-V-java.util.function.BiFunction-
+		//it uses Functional interface BiFunction
+		HashMap<String, Long> res = new HashMap<>();
+		for(String string: strings) {
+			res.merge(string, 1l, Long::sum);
+		}
+		return res;
+	}
+	/**
+	 * Anagram of a string is another string containing all the symbols
+	 * from the string, but in different order (see the test cases)
+	 * @param string
+	 * @param anagram
+	 * @return true if the second parameter presents an anagram
+	 *  of the string from the first parameter, otherwise false
+	 */
+	public static boolean isAnagram(String string, String anagram) {
+
+		if (string.length() != anagram.length()) {
+			return false;
 		}
 
-		return map;
-	}
+		if (string == anagram){
+			return false;
+		}
 
+		char[] chars1 = string.toCharArray();
+		char[] chars2 = anagram.toCharArray();
+
+		Arrays.sort(chars1);
+		Arrays.sort(chars2);
+
+		return Arrays.equals(chars1, chars2);
+
+	}
 
 }
--- src/telran/interview/test/InterviewTasksTest.java
+++ src/telran/interview/test/InterviewTasksTest.java
@@ -5,16 +5,37 @@ import static telran.interview.InterviewTasks.*;
 
 import java.util.HashMap;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InterviewTasksTest {
-
+	private static final int N_NUMBERS = 100000;
+	int[] bigArray = new int[N_NUMBERS];
+	@Test
+	@DisplayName("performance O[N] is Sum2 ")
+	void isSum2PerformanceTest() {
+		//isSum2 complexity O[N]
+		isSum2(bigArray, 1);
+	}
+	@Test
+	@DisplayName("performance O[N^2] is Sum2N2 ")
+	void isSum2N2PerformanceTest() {
+		//isSum2N2 complexity O[N^2]
+		isSum2N2(bigArray, 1);
+	}
 	@Test
 	void isSum2Test() {
 		int [] ar1 = {1000, 300, -200, 20, 40, -10};
-		assertTrue(isSum2(ar1, 800));
-		assertTrue(isSum2(ar1, 990));
-		assertFalse(isSum2(ar1, 50));
+		assertTrue(isSum2(ar1, 800));//1000 + (-200) = 800
+		assertTrue(isSum2(ar1, 990));//1000 + (-10) = 990
+		assertFalse(isSum2(ar1, 50)); //no two number with the given sum
+	}
+	@Test
+	void isSum2N2Test() {
+		int [] ar1 = {1000, 300, -200, 20, 40, -10};
+		assertTrue(isSum2N2(ar1, 800));//1000 + (-200) = 800
+		assertTrue(isSum2N2(ar1, 990));//1000 + (-10) = 990
+		assertFalse(isSum2N2(ar1, 50)); //no two number with the given sum
 	}
 	@Test
 	void getMaxPositiveWithNegativeValueTest() {
@@ -26,13 +47,26 @@ class InterviewTasksTest {
 	@Test
 	void getMapOccurrencesTest() {
 		String[] strings = {
-			"lpm", "ab", "a", "lpm", "a", "aa", "lpm"
+				"lpm", "ab", "a", "lpm", "a", "aa", "lpm"
 		};
 		HashMap<String, Long> mapOccurrences = getMapOccurrences(strings);
 		assertEquals(3, mapOccurrences.get("lpm"));
 		assertEquals(2, mapOccurrences.get("a"));
 		assertEquals(1, mapOccurrences.get("ab"));
 		assertEquals(1, mapOccurrences.get("aa"));
+	}
+	@Test
+	void isAnagramTest() {
+		String string = "yellow";
+		assertTrue(isAnagram(string, "wolely"));
+		assertTrue(isAnagram(string, "elolwy"));
+		assertTrue(isAnagram(string, "lowely"));
+		assertTrue(isAnagram(string, "ollwey"));
+		assertFalse(isAnagram(string, "wolelw")); //not y
+		assertFalse(isAnagram(string, string));//the same order
+		assertFalse(isAnagram(string, "wglely"));//g is not from the string
+		assertFalse(isAnagram(string, "wolye"));//must be two 'l'
+
 	}
 
 }
