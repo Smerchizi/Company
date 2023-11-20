8d24759c26f839a2b02940da5308fe189bba5362
Smerchizi <smerch1533@gmail.com>
2023-11-20 21:18:45 +0200

    update

--- src/telran/view/InputOutput.java
+++ src/telran/view/InputOutput.java
@@ -1,4 +1,24 @@
 package telran.view;
 
+import java.util.function.Function;
+
 public interface InputOutput {
+    String readString(String prompt);
+    void write(String str);
+    default void writeLine(String str){
+        write(str + "\n");
+    }
+    default <T> T readObject(String prompt, String errorPromt, Function<String, T> mapper){
+        boolean running = false;
+        do {
+            running = false;
+            try {
+
+            }
+            catch (Exception e){
+                running = true;
+            }
+        }while (running);
+        return null;
+    }
 }
